0fd1316d905a01d9b0f8cc501f3d47d144bf6027
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-12-20 23:13:26 +0530

    new air drop contract.

--- www/conf.js
+++ www/conf.js
@@ -1,3 +1,3 @@
-contractAddress = '0x803631F30592d2769BB87073bf820bFf9481D8c7';
+contractAddress = '0xC6ecDEB56F910554Ea219e6434CBE8266E14afA0';
 tokenAddress = '0x3e5400d53c4167fc8f0fb81fa8b0104c6d279775';
 provider = 'https://ropsten.infura.io';
